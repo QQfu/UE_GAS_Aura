2b7d6801a2bd4ca4729b89783f3ddfd7881d7453
qingquan <fuqingquandl@163.com>
2024-04-06 19:06:07 +0800

    Init GameModeBase Add Spring Arm and Camera

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	/*
+	 * top-down general settings
+	 */
+	//bOrientRotationToMovement: If true, rotate the Character toward the direction of acceleration, using RotationRate as the rate of rotation change.
+	//Overrides UseControllerDesiredRotation. Normally you will want to make sure that other settings are cleared, such as bUseControllerRotationYaw on the Character.
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	//RotationRate: Change in rotation per second, used when UseControllerDesiredRotation or OrientRotationToMovement are true. Set a negative value for infinite rotation rate and instant turns.
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	//bConstrainToPlane: If true, movement will be constrained to a plane.
+	GetCharacterMovement()->bConstrainToPlane = true;
+	//bSnapToPlaneAtStart: If true and plane constraints are enabled, then the updated component will be snapped to the plane when first attached.
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -35,4 +36,38 @@ void AAuraPlayerController::BeginPlay()
 	//SetHideCursorDuringCapture: Whether to hide the cursor during temporary mouse capture caused by a mouse down
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	/*
+	 * 绑定Enhanced Input Actions
+	 */
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(InputAction_Move, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+/*
+ * 1. 找到当前Controller的前方和右方向量
+ * 2. 在Controlled Pawn中利用AddMovementInput方法进行移动
+ */
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	//1.找到当前Controller的前方和右方向量
+	const FRotator ControllerRotation = GetControlRotation();
+	const FRotator YawRotation(0.f, ControllerRotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	//2.在Controlled Pawn中利用AddMovementInput方法进行移动
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+
+	if(APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
 }
